ff7e2f6c465448fbf0ff719ec91898a1eba18094
sanjaykrishna1203 <sanjaykrishna1203@gmail.com>
2023-01-31 10:56:09 +0530

    Added Registrartion and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './body/register/register.component';
+import { LoginComponent } from './body/login/login.component';
+import { HomeComponent } from './body/home/home.component';
+import { registerLocaleData } from '@angular/common';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'register', component:RegisterComponent},
+  {path:'login', component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HeaderPopupComponent } from './header/header-popup/header-popup.component';
+import { BodyComponent } from './body/body.component';
+import { LoginComponent } from './body/login/login.component';
+import { RegisterComponent } from './body/register/register.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './body/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HeaderPopupComponent
+    HeaderPopupComponent,
+    BodyComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
